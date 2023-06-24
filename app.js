86f1527f493b49b41a45b995bfd66c7564ce8ffc
Amrahvn <amrahvn@code.edu.az>
2023-06-24 04:56:53 +0400

    add

---
+++ app.js
@@ -0,0 +1,16 @@
+
+
+let button=document.querySelector('#btn')
+
+button.addEventListener('click',function(){
+
+     
+     
+
+    if(document.getElementById('head').style.display==='none'){
+        document.getElementById('head').style.display='inline-block'
+    }else{
+        document.getElementById('head').style.display==='none'
+    }
+})
+
